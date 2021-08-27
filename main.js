99ba62fa323d14c029622c245cb3ce72f250b3ef
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-08-26 22:34:50 -0300

    Projeto

---
+++ main.js
@@ -0,0 +1,6 @@
+const inputBox = document.querySelector('.inputField input');
+const addBtn = document.querySelector('.inputField button');
+
+inputBox.onkeyup = () => {
+    let userData = inputBox.value;
+}
